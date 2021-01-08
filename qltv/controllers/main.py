4c4b2f4d9f8f2d627c399dcdc4eda2eeb6aaafaa
chien <chien28061998@gmail.com>
2021-01-08 17:18:54 +0700

    [Fix]  - update them sudo vao trong search

--- qltv/controllers/main.py
+++ qltv/controllers/main.py
@@ -6,5 +6,5 @@ class MuonSach(http.Controller):
 
     @http.route('/danh_muc_sach', type='http', auth='public', website=True)
     def muon_sach(self, **kwargs):
-        ms = request.env['danh.muc.sach'].search([])
+        ms = request.env['danh.muc.sach'].sudo().search([])
         return request.render('qltv.sach_page', {'ms': ms})

4c0522d2dcb0af32cb017fc7781ab724e4070e46
doduychien <chien28061998@gmail.com>
2021-02-20 09:45:46 +0700

    update phan khai bao in nhieu gia tri trong excel

--- qltv/report/report_excel.py
+++ qltv/report/report_excel.py
@@ -7,16 +7,26 @@ class QLTVxlsx(models.AbstractModel):
 
     def generate_xlsx_report(self, workbook, data, lines):
         format1 = workbook.add_format({'font_size': 14, 'align': 'vcenter', 'bold': True})
-        format2 = workbook.add_format({'font_size': 10, 'align': 'vcenter', })
+        format2 = workbook.add_format({'font_size': 10, 'align': 'vcenter',})
         sheet = workbook.add_worksheet('Thẻ mượn sách')
 
         # label
         sheet.write(0, 0, 'Người mượn sách', format1)
         sheet.write(0, 1, 'SĐT', format1)
         sheet.write(0, 2, 'Tên sách', format1)
-        # value 1 gia tri
+
+         # 1 gia tri
         # sheet.write(1, 0, lines.partner_id.name, format2)
         # sheet.write(1, 1, lines.phone, format2)
         # sheet.write(1, 2, lines.sach_id.name, format2)
+
+        # #nhieu gia tri
+        row = 1
+        for i in lines:
+            sheet.write(row, 0, i.partner_id.name, format2)
+            sheet.write(row, 1, i.phone, format2)
+            sheet.write(row, 2, i.sach_id.name, format2)
+            row += 1
+
 
 
